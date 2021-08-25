11ea5e06e310baa741884b048f62d862604adb47
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:24:37 +0300

    Add files via upload

---
+++ HM_7/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_7
+{
+    class Car
+    {
+        public int Price_ { get; set; }
+
+        public Car(int price_)
+        {
+            Price_ = price_;
+        }
+    }
+}
---
+++ HM_7/Check_Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace HM_7
+{
+    class Check_Class
+    {
+        public string   check_obj_name(object obj)
+        {
+            if (obj.GetType().GetProperty("Name") != null)
+            {
+                return "The obj type is " + obj.GetType().Name.ToString()+"\n"+"The Name property is "+ obj.GetType().GetProperty("Name").GetValue(obj).ToString();
+            }
+            else 
+            {
+                return "This class dosent have a Name property";
+            }           
+        }
+         
+       
+    }
+}
