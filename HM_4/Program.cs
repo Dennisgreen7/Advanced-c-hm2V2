487ee8b1410aa0bebae3ac5db8842ad37b66900d
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:15:47 +0300

    Add files via upload

---
+++ HM_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class_Int num = new Class_Int();
+            Class_Creatorcs<Class_Int> a = new Class_Creatorcs<Class_Int>();
+            a.new_class.Id_ = 5;
+      
+        }
+    }
+}
