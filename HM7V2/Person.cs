5285c1ebec9b732f1ff5f7214ba10f5cc5dda151
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:26:55 +0300

    Add files via upload

---
+++ HM7V2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM7V2
+{
+    class Person : Interface1
+    {
+        public string Name_ { get; set; }
+
+        public Person(string name_)
+        {
+            Name_ = name_;
+        }
+    }
+}
---
+++ HM7V2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM7V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person a = new Person("Dennis");
+            Class1<Person> b = new Class1<Person>();
+            b.get_info(a);
+            Console.ReadLine();
+        }
+    }
+}
