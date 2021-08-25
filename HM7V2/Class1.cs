90e0085f46a71a380eadb0a79066952c8c80e5cd
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:26:30 +0300

    Add files via upload

---
+++ HM7V2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM7V2
+{
+    class Class1<T> where T : Interface1
+    {
+
+        public void  get_info(T obj)
+        {
+            Console.WriteLine("Class type is " + obj.GetType().Name.ToString()+" name property = "+obj.Name_ );
+        }
+    }
+}
