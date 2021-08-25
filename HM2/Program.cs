8f9ea06442ce17b704ceb87d1f0802c1e90e714d
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:11:47 +0300

    Add files via upload

---
+++ HM2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnly<int> a = new ReadOnly<int>();
+            a.IsReadOnly = true;
+            a.AddToList(1);
+            a.AddToList(2);
+            a.ToString();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HM2/ReadOnly.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2
+{
+    class ReadOnly<T>
+    {
+        private T[] list_ = new T[10];
+
+        public bool IsReadOnly { get; set; }
+        private int Count_ = 0;
+        public void AddToList(T data_)
+        {
+            try
+            {
+                if (IsReadOnly)
+                {
+                    throw new InvalidOperationException("when readonly is on you cant add to list");
+                }
+                else
+                {
+                    list_[Count_++] = data_;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
