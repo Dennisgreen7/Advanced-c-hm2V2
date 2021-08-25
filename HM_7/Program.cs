9a826546ad2fa2d4ab6b0a2da6263eb57f358544
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:25:14 +0300

    Add files via upload

---
+++ HM_7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check_Class test = new Check_Class();
+            Person d = new Person("Dennis");
+            Car c = new Car(1);
+            string str1 = test.check_obj_name(d);
+            string str2 = test.check_obj_name(c);
+            Console.WriteLine(str1+"\n"+str2);
+            Console.ReadLine();
+        }
+    }
+}
