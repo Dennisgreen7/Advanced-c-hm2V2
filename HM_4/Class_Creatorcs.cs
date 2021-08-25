75bd43d7b8737800340dc70a41b3f9e6e0fc0721
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2021-08-25 18:15:29 +0300

    Add files via upload

---
+++ HM_4/Class_Creatorcs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Class_Creatorcs<T>where T:IClassId,new()
+    {
+        public T new_class { get; set; }
+        public void  create_new_class()
+        {
+            new_class = new T();
+        }
+    }
+}
---
+++ HM_4/Class_Int.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM_4
+{
+    class Class_Int :IClassId
+    {
+        public int Id_ { get ; set; }
+    }
+}
